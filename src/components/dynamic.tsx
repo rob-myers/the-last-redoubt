4c596b7775ada548e8714f104f7c9ed100ae219d
Rob Myers <me.robmyers@gmail.com>
2022-07-14 14:01:53 +0100

    feat: link loadables

--- src/components/dynamic.tsx
+++ src/components/dynamic.tsx
@@ -1,16 +1,19 @@
 import loadable from '@loadable/component'
+import type ActualLayout from './page/Layout';
+import type ActualCodeEditor from './code/CodeEditor';
+import type ActualTerminal from './sh/Terminal';
 
 export const Layout = loadable(
   () => import('./page/Layout'),
   { ssr: false },
-);
+) as typeof ActualLayout;
 
 export const CodeEditor = loadable(
   () => import('./code/CodeEditor'),
   { ssr: false },
-);
+) as typeof ActualCodeEditor;
 
 export const Terminal = loadable(
   () => import('./sh/Terminal'),
   { ssr: false },
-);
+) as typeof ActualTerminal;
