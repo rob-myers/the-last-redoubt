535d616a486698c0f6829356a96dc739c1f99702
Rob Myers <me.robmyers@gmail.com>
2022-08-02 22:26:50 +0100

    feat: add web animation api polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,10 +2,15 @@ import "src/components/globals.css"
 import "flexlayout-react/style/light.css"
 import "xterm/css/xterm.css"
 
-import ResizeObserver from 'resize-observer-polyfill';
+//#region polyfill
 
+import ResizeObserver from 'resize-observer-polyfill';
 if (!window.ResizeObserver) {
   window.ResizeObserver = ResizeObserver;
 }
+
+import('web-animations-js');
+
+//#endregion
 
 export { wrapPageElement } from "./src/components/page/Root";
