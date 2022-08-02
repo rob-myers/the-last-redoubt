d96f1eae81aa2f0ea19dd21219864a6b86bf6bd0
Rob Myers <me.robmyers@gmail.com>
2022-08-02 22:14:36 +0100

    feat: add ResizeObserver polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,10 @@ import "src/components/globals.css"
 import "flexlayout-react/style/light.css"
 import "xterm/css/xterm.css"
 
+import ResizeObserver from 'resize-observer-polyfill';
+
+if (!window.ResizeObserver) {
+  window.ResizeObserver = ResizeObserver;
+}
+
 export { wrapPageElement } from "./src/components/page/Root";
