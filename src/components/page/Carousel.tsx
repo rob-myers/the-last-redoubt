a2c3586819a72b612a24bf63683c564fa5146413
Rob Myers <me.robmyers@gmail.com>
2022-09-03 10:44:17 +0100

    feat: clean carousel

--- src/components/page/Carousel.tsx
+++ src/components/page/Carousel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
+import { Navigation, SwiperOptions } from "swiper";
 
 export default function Carousel(props: Props) {
   return (
@@ -11,20 +11,8 @@ export default function Carousel(props: Props) {
         modules={[Navigation]}
         navigation
         spaceBetween={20}
-        // centeredSlides
-        breakpoints={{
-          300: {
-            slidesPerView: 1,
-          },
-          450: {
-            slidesPerView: 2,
-          },
-          900: {
-            slidesPerView: 3,
-          },
-        }}
+        breakpoints={props.breakpoints}
         style={{ height: props.height }}
-        // onResize={() => setSlidesPerView(window.matchMedia('(max-width: 700px) and (min-width: 600px)').matches ? 1 : 2)}
       >
         {props.items.map(({ src, label }) =>
           <SwiperSlide key={src}>
@@ -43,14 +31,7 @@ export default function Carousel(props: Props) {
 
 interface Props {
   baseSrc?: string;
+  breakpoints?: SwiperOptions['breakpoints'];
   height: number;
   items: { src: string; label: string; }[];
-}
-
-interface State {
-  computeResponsive(): void;
-  rootEl: HTMLElement;
-  slidesPerView: number;
-  /** Seems to mean _minimum_ spaceBetween. */
-  spaceBetween: number;
 }
