dbf8322321d00265f90e0d82f014108c59292a69
Rob Myers <me.robmyers@gmail.com>
2022-09-03 14:39:18 +0100

    feat: fix 404 spacing on mobile

--- src/templates/404.jsx
+++ src/templates/404.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { css } from '@emotion/css';
+import { useLocation } from '@reach/router';
 
 /**
  * - This file should be JSX, because `path.resolve`d by gatsby-node.js.
  * - Didn't work when put inside src/components/page
  */
 export default function NotFoundPage() {
-  return <><b>404</b>: requested path not found.</>;
+
+  const location = useLocation();  
+
+  return (
+    <main className={rootCss}>
+      <article>
+        <p>
+          Status 404: <span>{location.href}</span> was not found.
+        </p>
+      </article>
+    </main>
+  );
 }
+
+const rootCss = css`
+  margin: 32px 0;
+  @media (max-width: 600px) {
+    margin: 32px 8px;
+  }
+
+  p {
+    font-weight: 600;
+  }
+  span {
+    font-weight: 300;
+    color: #444;
+  }
+`;
