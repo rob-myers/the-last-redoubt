07a62c771f5a6467016199ff3dc73bdc480f2fc1
Rob Myers <me.robmyers@gmail.com>
2022-09-07 16:50:37 +0100

    feat: clean

--- src/components/page/InnerTable.tsx
+++ src/components/page/InnerTable.tsx
@@ -4,9 +4,7 @@ export default function InnerTable(props: React.PropsWithChildren<{}>) {
 
   const rows = React.Children.toArray(props.children).map(row => (
     React.Children.toArray((row as any)?.props?.children)
-  ).filter(Boolean)); // Ignore incorrectly formatted rows
-
-  console.log({rows})
+  ));
 
   return (
     <table
@@ -22,18 +20,6 @@ export default function InnerTable(props: React.PropsWithChildren<{}>) {
             {row.map((col, i) => <td key={i}>{col}</td>)}
           </tr>
         ))}
-        {/* <tr>
-          <td>[The Night Land](https://en.wikipedia.org/wiki/The_Night_Land "@new-tab")</td>
-          <td>An early sci-fi story (1912).</td>
-        </tr>
-        <tr>
-          <td>[Theravada Buddhism](https://en.wikipedia.org/wiki/Theravada "@new-tab")</td>
-          <td>The religion of the last of humanity.</td>
-        </tr>
-        <tr>
-          <td> [Fallout 1, 2](https://en.wikipedia.org/wiki/Fallout_(video_game) "@new-tab")</td>
-          <td>Oblique turn-based RPGs.</td>
-        </tr> */}
       </tbody>
     </table>
   );
