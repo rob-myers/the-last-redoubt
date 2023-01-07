36d9b256c84af257cc74dcbac95bf4831cbefd62
robmyers <me.robmyers@gmail.com>
2023-01-07 16:37:06 +0000

    feat: towards extracting contact points

--- src/scripts/npc-json.js
+++ src/scripts/npc-json.js
@@ -7,7 +7,7 @@
  * - {folder} relative to media/NPC should contain {folder}.sifz
  * - Examples:
  *  - yarn npc-json first-anim
- *  - yarn npc-json first-anim walk
+ *  - yarn npc-json first-anim walk 🚧
  */
 
 import path from 'path';
@@ -15,8 +15,9 @@ import fs from 'fs';
 import zlib from 'zlib';
 import xml2js from 'xml2js';
 import { writeAsJson } from '../projects/service/file';
-import { error } from '../projects/service/log';
-import { Rect } from '../projects/geom';
+import { error, warn } from '../projects/service/log';
+import { Rect, Vect } from '../projects/geom';
+import { assertDefined } from '../projects/service/generic';
 // import { mapValues } from '../projects/service/generic';
 // import * as npcService from '../projects/service/npc';
 
@@ -34,6 +35,8 @@ const staticAssetsDir = path.resolve(__dirname, '../../static/assets');
 const npcJsonOutputDir = path.resolve(staticAssetsDir, 'npc');
 // fs.mkdirSync(npcJsonOutputDir, { recursive: true });
 
+const zoom = 2;
+const baseAabb = new Rect(0, 0, 128, 128);
 
 async function main() {
     // Obtain XML by decompressing Synfig .sifz file
@@ -42,37 +45,69 @@ async function main() {
     const parser = new xml2js.Parser();
     /** @type {{ canvas: ParsedInnerSynfig }} */
     const synfigJson = await parser.parseStringPromise(synfigXml);
-    const { $: topDollar, name: [npcName], keyframe } = synfigJson.canvas;
+    const { $: topDollar, name: [npcName], keyframe, layer } = synfigJson.canvas;
 
     const totalFrameCount = parseInt(topDollar['end-time']) + 1;
-    console.log({totalFrameCount})
+
+    // Internal coords will be relative to this "space"
+    const viewBoxCoords = topDollar['view-box'].split(' ').map(Number);
+    const viewTopLeft = new Vect(viewBoxCoords[0], viewBoxCoords[1]);
+    const viewBottomRight = new Vect(viewBoxCoords[2], viewBoxCoords[3]);
+
+    const contactsLayer = layer.find(x => x.$.desc === 'ContactPoints');
+    if (!contactsLayer) {
+      warn(`${sifzInputDir}: ContactPoints layer not found )`);
+    }
 
     /** @type {NPC.ParsedNpc['animLookup']} */
     const animLookup = {};
     keyframe.forEach(({ _: animName, $: { time } }, i) => {
-        animLookup[animName] = {
-            animName,
-            frameCount: keyframe[i + 1]
-              ? parseInt(keyframe[i + 1].$.time) - parseInt(time)
-              : totalFrameCount - parseInt(time),
-            aabb: new Rect, // 🚧
-            contacts: [], // 🚧
-            deltas: [], // 🚧
-            totalDist: 0, // 🚧
+
+      if (contactsLayer) {// Contact points
+        const canvas = assertDefined(contactsLayer.param.find(x => x.$.name === 'canvas')?.canvas?.[0]);
+        const leftLayer = canvas.layer.find(x => x.$.desc === 'LeftContact');
+        const rightLayer = canvas.layer.find(x => x.$.desc === 'RightContact');
+
+        const leftWay = {
+          topLeft: leftLayer?.param.find(x => x.$.name === 'point1')?.animated?.[0].waypoint,
+          bottomRight: leftLayer?.param.find(x => x.$.name === 'point2')?.animated?.[0].waypoint,
         };
-    });
+        const rightWay = {
+          topLeft: rightLayer?.param.find(x => x.$.name === 'point1')?.animated?.[0].waypoint,
+          bottomRight: rightLayer?.param.find(x => x.$.name === 'point2')?.animated?.[0].waypoint,
+        };
+
+        // 🚧
+        console.log({
+          leftWay,
+          rightWay,
+        });
+      }
+
+      animLookup[animName] = {
+          animName,
+          frameCount: keyframe[i + 1]
+            ? parseInt(keyframe[i + 1].$.time) - parseInt(time)
+            : totalFrameCount - parseInt(time),
+          aabb: baseAabb.clone().scale(zoom).json,
+
+          contacts: [], // 🚧
+          deltas: [], // 🚧
+          totalDist: 0, // 🚧
+      };
+  });
 
     /** @type {NPC.ParsedNpc} */
     const npcJson = {
         npcName,
         animLookup,
-        zoom: 2,
+        zoom,
     };
 
     writeAsJson(npcJson, path.resolve(npcJsonOutputDir, `${sifzFolder}.json`));
 
     // ⛔️ debug only
-    // writeAsJson(synfigJson, path.resolve(npcJsonOutputDir, `${sifzFolder}.orig.json`));
+    writeAsJson(synfigJson, path.resolve(npcJsonOutputDir, `${sifzFolder}.orig.json`));
 }
 
 main();
@@ -119,4 +154,12 @@ main();
  * @property {{ name: string; }} $
  * @property {{ $: { value: string; } }[]} [real]
  * @property {{ $: { value: string; static?: string; } }[]} [integer]
+ * @property {Pick<ParsedInnerSynfig, 'layer'>[]} [canvas]
+ * @property {SynfigAnimParam[]} [animated]
+ */
+
+/**
+ * @typedef SynfigAnimParam
+ * @property {{ type: 'vector' }} $
+ * @property {{ $: { time: string; before: string; after: string }; vector: { $: { guid: string; }; x: string[]; y: string[] }[] }[]} waypoint
  */
