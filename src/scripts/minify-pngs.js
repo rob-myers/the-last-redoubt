432f8f0385fdec9424ddb432afa40d5d4fed209b
robmyers <me.robmyers@gmail.com>
2023-02-16 11:56:57 +0000

    feat: fix webp conversion

--- src/scripts/minify-pngs.js
+++ src/scripts/minify-pngs.js
@@ -25,40 +25,49 @@ if (!srcDir || !fs.existsSync(srcDir) || (extra && (extra !== 'webp'))) {
   `);
   process.exit(1);
 }
-//#region optipng
-// https://tinypng.com/ was often much better.
-// if (childProcess.execSync(`optipng --version | grep OptiPNG  >/dev/null && echo $?`).toString().trim() !== '0') {
-//   error("error: please install optipng e.g. `brew install optipng`");
-//   process.exit(1);
-// }
 
-// info(`applying parallel \`optipng\` to directory ${srcDir}`);
-// childProcess.execSync(`
-//   time find ${path.join(`'${srcDir}'`, '*.png')} -print0 |
-//     xargs -0 -n 1 -P 20 optipng
-// `);
-//#endregion
+main();
 
-// Create webp files first
-if (extra === 'webp') {
-  childProcess.execSync('yarn pngs-to-webp')
+async function main() {
+  //#region optipng
+  // https://tinypng.com/ was often much better.
+  // if (childProcess.execSync(`optipng --version | grep OptiPNG  >/dev/null && echo $?`).toString().trim() !== '0') {
+  //   error("error: please install optipng e.g. `brew install optipng`");
+  //   process.exit(1);
+  // }
+
+  // info(`applying parallel \`optipng\` to directory ${srcDir}`);
+  // childProcess.execSync(`
+  //   time find ${path.join(`'${srcDir}'`, '*.png')} -print0 |
+  //     xargs -0 -n 1 -P 20 optipng
+  // `);
+  //#endregion
+
+  // Create webp files first
+  if (extra === 'webp') {
+    await /** @type {Promise<void>} */ (new Promise(resolve => {
+      const proc = childProcess.spawn(`yarn`, ['pngs-to-webp', srcDir]);
+      proc.stdout.on('data', (data) => console.log({ key: 'pngs-to-webp' }, data.toString()));
+      proc.stdout.on('exit', () => resolve());
+    }));
+  }
+
+  //#region pngquant
+  if (childProcess.execSync(`pngquant --help | grep pngquant  >/dev/null && echo $?`).toString().trim() !== '0') {
+    error("error: please install pngquant e.g. `brew install pngquant`");
+    process.exit(1);
+  }
+
+  info(`applying parallel \`pngquant\` to directory ${srcDir}`);
+  childProcess.execSync(`
+    time find ${path.join(`'${srcDir}'`, '*.png')} -print0 |
+      xargs -0 -n 1 -P 20 pngquant -f --quality=80
+  `);
+
+  for (const fileName of fs.readdirSync(srcDir).filter(x => x.endsWith('-fs8.png'))) {
+    const filePath = path.resolve(srcDir, fileName);
+    fs.renameSync(filePath, filePath.replace( /^(.*)(-fs8)\.png$/, '$1.png'));
+  }
+  //#endregion
+
 }
-
-//#region pngquant
-if (childProcess.execSync(`pngquant --help | grep pngquant  >/dev/null && echo $?`).toString().trim() !== '0') {
-  error("error: please install pngquant e.g. `brew install pngquant`");
-  process.exit(1);
-}
-
-info(`applying parallel \`pngquant\` to directory ${srcDir}`);
-childProcess.execSync(`
-  time find ${path.join(`'${srcDir}'`, '*.png')} -print0 |
-    xargs -0 -n 1 -P 20 pngquant -f --quality=80
-`);
-
-for (const fileName of fs.readdirSync(srcDir).filter(x => x.endsWith('-fs8.png'))) {
-  const filePath = path.resolve(srcDir, fileName);
-  fs.renameSync(filePath, filePath.replace( /^(.*)(-fs8)\.png$/, '$1.png'));
-}
-//#endregion
-
