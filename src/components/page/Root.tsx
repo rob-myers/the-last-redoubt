f2c412f165d687d1ab4737752256ce17f46b64b0
robmyers <me.robmyers@gmail.com>
2023-03-26 11:39:49 +0100

    feat: migrate StaticQuery

--- src/components/page/Root.tsx
+++ src/components/page/Root.tsx
@@ -1,5 +1,5 @@
 import type { WrapPageElementBrowserArgs, WrapPageElementNodeArgs } from "gatsby";
-import { StaticQuery, graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 import { Helmet } from "react-helmet";
 import { QueryClientProvider } from "react-query";
@@ -25,68 +25,56 @@ export function wrapPageElement({
     props.pageContext?.frontmatter
   ) as FrontMatter | undefined;
 
+  const allFrontMatter = useStaticQuery(graphql`
+    query { allMdx {
+      edges { node { frontmatter {
+        key
+        date
+        icon
+        giscusTerm
+        info
+        label
+        navGroup
+        next
+        path
+        prev
+        tags
+      } } } }
+    }
+  `) as AllFrontMatter;
+
   return (
     <>
+      <RootHooks
+        allFrontMatter={allFrontMatter}
+        frontMatter={frontMatter}
+      />
       <Helmet>
         <title>
           {siteTitle}
         </title>
       </Helmet>
 
-      <StaticQuery
-        query={graphql`
-          query { allMdx {
-            edges { node { frontmatter {
-              key
-              date
-              icon
-              giscusTerm
-              info
-              label
-              navGroup
-              next
-              path
-              prev
-              tags
-            } } } }
+      <QueryClientProvider client={queryClient} >
+        <Nav frontmatter={frontMatter} />
+        <Main>
+          {frontMatter
+            ? <>
+                <Article frontmatter={frontMatter}>{element}</Article>
+                <NextArticle frontMatter={frontMatter}/>
+              </>
+            : element
           }
-      `}
-        render={(allFrontMatter: AllFrontMatter) => {
-
-          React.useMemo(() => {
-            clearAllBodyScrollLocks();
-            useSiteStore.api.setArticleKey(frontMatter?.key);
-            useSiteStore.api.initiate(allFrontMatter);
-          }, [frontMatter]);
-          
-          React.useEffect(() => {
-            useSiteStore.api.initiateBrowser();
-          }, []);
-
-          return (
-            <QueryClientProvider client={queryClient} >
-              <Nav frontmatter={frontMatter} />
-              <Main>
-                {frontMatter
-                  ? <>
-                      <Article frontmatter={frontMatter}>{element}</Article>
-                      <NextArticle frontMatter={frontMatter}/>
-                    </>
-                  : element
-                }
-                <Comments
-                  id="comments"
-                  term={frontMatter?.giscusTerm || frontMatter?.path || 'fallback-discussion'}
-                />
-              </Main>
-              <Portals />
-              <ReactQueryDevtools // Performance issue after HMRs?
-                initialIsOpen={false}
-              />
-            </QueryClientProvider>
-          );
-        }}
-      />
+          <Comments
+            id="comments"
+            term={frontMatter?.giscusTerm || frontMatter?.path || 'fallback-discussion'}
+          />
+        </Main>
+        <Portals />
+        <ReactQueryDevtools // Performance issue after HMRs?
+          initialIsOpen={false}
+        />
+      </QueryClientProvider>
 
     </>
   );
@@ -94,4 +82,21 @@ export function wrapPageElement({
 
 export interface FrontMatterProps {
   frontmatter?: FrontMatter;
+}
+
+function RootHooks(props: {
+  allFrontMatter: AllFrontMatter;
+  frontMatter?: FrontMatter;
+}) {
+  React.useMemo(() => {
+    clearAllBodyScrollLocks();
+    useSiteStore.api.setArticleKey(props.frontMatter?.key);
+    useSiteStore.api.initiate(props.allFrontMatter);
+  }, [props.frontMatter]);
+  
+  React.useEffect(() => {
+    useSiteStore.api.initiateBrowser();
+  }, []);
+
+  return null;
 }
