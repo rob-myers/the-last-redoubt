99a4cbb3c28d0065630da86d3d93292e6e8353d3
robmyers <me.robmyers@gmail.com>
2023-09-27 16:49:19 +0100

    feat: browser warn/info/error has colour

--- src/projects/service/log.js
+++ src/projects/service/log.js
@@ -1,20 +1,33 @@
 import chalk from "chalk";
+const isBrowser = typeof window !== 'undefined';
 
 /**
  * @param  {...any} args 
  */
  export function error(...args) {
-  console.error('ERROR', chalk.red(...args));
+  if (isBrowser) {
+    console.info('%cERROR', 'background: #ff0000', ...args);
+  } else {
+    console.info(chalk.red('ERROR:', ...args));
+  }
 }
 /**
  * @param  {...any} args 
  */
 export function info(...args) {
-  console.info('INFO', chalk.blueBright(...args));
+  if (isBrowser) {
+    console.info('%cINFO', 'color: #7777ff', ...args);
+  } else {
+    console.info(chalk.blueBright('INFO:', ...args));
+  }
 }
 /**
  * @param  {...any} args 
  */
 export function warn(...args) {
-  console.warn('WARN', chalk.yellowBright(...args));
+  if (isBrowser) {
+    console.info('%cWARN', 'color: #ffff00', ...args);
+  } else {
+    console.info(chalk.yellowBright('WARN:', ...args));
+  }
 }
