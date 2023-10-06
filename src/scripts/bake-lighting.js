e4f5010ed2f0804ceda43f10034da778bed5ed28
robmyers <me.robmyers@gmail.com>
2023-10-06 16:10:50 +0100

    feat: lights are no longer yellow

--- src/scripts/bake-lighting.js
+++ src/scripts/bake-lighting.js
@@ -59,7 +59,7 @@ if (!fs.existsSync(geomorphJsonPath)) {
   lightPolys.forEach((lightPoly, i) => {
     const { position, distance = defaultLightDistance } = lightSources[i];
     const gradient = ctxt.createRadialGradient(position.x, position.y, 1, position.x, position.y, distance);
-    gradient.addColorStop(0, '#ffffaa77');
+    gradient.addColorStop(0, '#ffffff77');
     gradient.addColorStop(1, "#00000000");
     ctxt.fillStyle = gradient;
     fillPolygons(ctxt, [lightPoly]);
