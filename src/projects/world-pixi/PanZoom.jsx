159118464ff572f15bf48a1acfa3b711194c1492
robmyers <me.robmyers@gmail.com>
2023-11-23 12:34:32 +0000

    feat: indentation

--- src/projects/world-pixi/PanZoom.jsx
+++ src/projects/world-pixi/PanZoom.jsx
@@ -13,85 +13,83 @@ import debounce from "debounce";
  * @param {React.PropsWithChildren<Props>} props 
  */
 export default function PanZoom(props) {
-  const state = useStateRef(
-    /** @type {() => State} */ () => ({
-      ready: true,
-      events: new Subject,
-      clickIds: [],
-      start: {
-        origin: undefined,
-        clientOrigin: undefined,
-        scale: 1,
-        distance: 0, // on canvas
-        epochMs: -1,
-      },
+  const state = useStateRef(/** @type {() => State} */ () => ({
+    ready: true,
+    events: new Subject,
+    clickIds: [],
+    start: {
+      origin: undefined,
+      clientOrigin: undefined,
+      scale: 1,
+      distance: 0, // on canvas
+      epochMs: -1,
+    },
 
-      viewport: /** @type {*} */ ({}),
-      input: /** @type {*} */ ({}),
-      transform: /** @type {*} */ ({}),
-      animate: /** @type {*} */ ({}),
+    viewport: /** @type {*} */ ({}),
+    input: /** @type {*} */ ({}),
+    transform: /** @type {*} */ ({}),
+    animate: /** @type {*} */ ({}),
 
-      getWorld(e) {
-        return state.transform.localTransform.applyInverse(e.global);
-      },
-      isFollowing() {
-        return !!state.viewport.plugins.get('follow');
-      },
-      isIdle() {
-        const { touches, isMouseDown } = state.input;
-        return touches.length === 0 && !isMouseDown && !state.viewport.zooming;
-      },
-      pointerdown(e) {
-        !state.isFollowing() && state.animate.complete(); // cancel?
-        const origin = state.getWorld(e);
-        state.start = {
-          origin,
-          clientOrigin: { x: e.client.x, y: e.client.y },
-          scale: state.viewport.scale.x,
-          distance: 0, // or getDistance(state.input.touches)
-          epochMs: Date.now(),
-        };
-      },
-      pointermove(e) {
-        if (e.nativeEvent.target === props.api.canvas) {
-          state.events.next({ key: 'pointermove', point: state.getWorld(e) });
-        } // ignore pointermove outside viewport
-      },
-      pointerup(e) {
-        if (!state.start.clientOrigin) {
-          return;
-        }
+    getWorld(e) {
+      return state.transform.localTransform.applyInverse(e.global);
+    },
+    isFollowing() {
+      return !!state.viewport.plugins.get('follow');
+    },
+    isIdle() {
+      const { touches, isMouseDown } = state.input;
+      return touches.length === 0 && !isMouseDown && !state.viewport.zooming;
+    },
+    pointerdown(e) {
+      !state.isFollowing() && state.animate.complete(); // cancel?
+      const origin = state.getWorld(e);
+      state.start = {
+        origin,
+        clientOrigin: { x: e.client.x, y: e.client.y },
+        scale: state.viewport.scale.x,
+        distance: 0, // or getDistance(state.input.touches)
+        epochMs: Date.now(),
+      };
+    },
+    pointermove(e) {
+      if (e.nativeEvent.target === props.api.canvas) {
+        state.events.next({ key: 'pointermove', point: state.getWorld(e) });
+      } // ignore pointermove outside viewport
+    },
+    pointerup(e) {
+      if (!state.start.clientOrigin) {
+        return;
+      }
 
-        const worldPoint = state.getWorld(e);
-        const distance = tempVect.copy(e.client).distanceTo(state.start.clientOrigin);
-        // console.log("pointerup", worldPoint);
+      const worldPoint = state.getWorld(e);
+      const distance = tempVect.copy(e.client).distanceTo(state.start.clientOrigin);
+      // console.log("pointerup", worldPoint);
 
-        state.events.next({
-          key: 'pointerup',
-          meta: {
-            distance,
-            longClick: (Date.now() - state.start.epochMs) >= longClickMs,
-            targetPos: worldPoint, // 🚧 remove?
-          },
-          point: worldPoint,
-          clickId: state.clickIds.pop(),
-        });
-      },
-      onPanEnd() {
-        state.isIdle() && state.events.next({ key: 'ui-idle' });
-      },
-      onZoomEnd: debounce(() => {
-        state.isIdle() && state.events.next({ key: 'ui-idle' });
-      }, 100),
-      viewportRef(vp) {
-        if (vp) {
-          state.viewport = vp;
-          state.input = vp.input;
-          state.transform = vp.transform;
-        }
-      },
-    })
-  );
+      state.events.next({
+        key: 'pointerup',
+        meta: {
+          distance,
+          longClick: (Date.now() - state.start.epochMs) >= longClickMs,
+          targetPos: worldPoint, // 🚧 remove?
+        },
+        point: worldPoint,
+        clickId: state.clickIds.pop(),
+      });
+    },
+    onPanEnd() {
+      state.isIdle() && state.events.next({ key: 'ui-idle' });
+    },
+    onZoomEnd: debounce(() => {
+      state.isIdle() && state.events.next({ key: 'ui-idle' });
+    }, 100),
+    viewportRef(vp) {
+      if (vp) {
+        state.viewport = vp;
+        state.input = vp.input;
+        state.transform = vp.transform;
+      }
+    },
+  }));
 
   React.useEffect(() => {
     const { viewport: vp, onPanEnd, onZoomEnd } = state;
@@ -109,7 +107,7 @@ export default function PanZoom(props) {
   return (
     <Viewport
       ref={state.viewportRef}
-      initScale={0.5}
+      // initScale={0.5}
       eventMode="dynamic"
       pointerdown={state.pointerdown}
       pointermove={state.pointermove}
