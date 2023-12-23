cbf006c71e3f173043261fb376a901ce1b6976bb
robmyers <me.robmyers@gmail.com>
2023-12-23 10:03:53 +0000

    feat: GmSprites now inside a Container

--- src/projects/world-pixi/GmSprites.jsx
+++ src/projects/world-pixi/GmSprites.jsx
@@ -1,29 +1,37 @@
-import React from "react";
-import { Sprite } from "@pixi/react";
+import React, { forwardRef } from "react";
+import { Sprite, Container } from "@pixi/react";
 
-/**
- * For Geomorphs, Doors, DebugWorld, Decor, FOV
- * @param {Props} props
- */
-export default function GmSprites(props) {
-  return <>
-    {props.gms.map((gm, gmId) => (
-      <Sprite
-        key={gmId}
-        {...gm.pixiTransform}
-        filters={props.filters ?? []}
-        eventMode="static"
-        width={gm.pngRect.width}
-        height={gm.pngRect.height}
-        texture={props.tex[gmId]}
-        visible={props.visible?.[gmId] ?? true}
-      />
-    ))}
-  </>;
-}
+const GmSprites = forwardRef(
+  /**
+   * For Geomorphs, Doors, DebugWorld, Decor, FOV
+   * @param {Props} props
+   */
+  function GmSprites(props, ref) {
+    return (
+      <Container ref={ref} name={props.name ?? 'gm-sprites'}>
+        {props.gms.map((gm, gmId) => (
+          <Sprite
+            key={gmId}
+            {...gm.pixiTransform}
+            filters={props.filters ?? []}
+            eventMode="static"
+            width={gm.pngRect.width}
+            height={gm.pngRect.height}
+            texture={props.tex[gmId]}
+            visible={props.visible?.[gmId] ?? true}
+          />
+        ))}
+      </Container>
+    );
+  }
+);
+
+export default GmSprites;
+
 
 /**
  * @typedef Props
+ * @property {string} [name]
  * @property {Geomorph.GeomorphDataInstance[]} gms
  * @property {import('@pixi/core').RenderTexture[]} tex Aligned to `gms`
  * @property {import('@pixi/core').Filter[]} [filters]
